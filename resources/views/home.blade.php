c675a8f537ee3a5eed13838ebdaa3456ac153d73
GianluigiCelentano <cgianluigi96@gmail.com>
2021-09-07 18:06:01 +0200

    create addColor/removeColor function

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,22 @@
         public $type;
         public $size;
         public $quality;
+        public $colors=[];
         public function __construct(string $type, int $size, string $quality) {
             $this->type = $type;
             $this->size = $size;
             $this->quality = $quality;
+        }
+        public function addColor(Products $color) {
+            $this->colors[] = $color;
+        }
+        public function removeColor(Products $color) {
+            $keyColorToRemove = array_search($color, $this->colors);
+            if($keyColorToRemove === false) {
+                throw new Exception("Il colore non è presente");
+            }
+    
+            unset($this->colors[$keyColorToRemove]);
         }
     }
     ?>
